b31fa5286460f9797aa2369a9a550cf58da96361
Seungha Lee <poopa8788@gmail.com>
2017-06-02 15:40:32 +0900

    add result page

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -40,8 +40,13 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the result of question %s."
-    return HttpResponse(response % question_id)
+    # detail.html 파일을 약간 수정해서 result.html을 만들고
+    # 질문에 대한 모든 선택사항의 선택수(votes)를 출력
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
