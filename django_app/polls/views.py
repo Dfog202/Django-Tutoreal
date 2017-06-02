2bcd1c2dd743ca3d99d641fd1f8fcdca2059c306
Seungha Lee <poopa8788@gmail.com>
2017-06-02 12:07:18 +0900

    add polls/detail.html, use template in detail view

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
@@ -18,7 +17,18 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # question_id가 pk인 Question객체를 가저와서
+    # context라는 이름을 가진 dict에 question이라는 키값으로 위 변수를 할당
+    # 이후 polls/detail.html 과 context를 랜더한 결과를 리턴
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist as e:
+        raise Http404('Question does not exist')
+        context = {
+            'question': question,
+        }
+    # polls/detail에서 해당 question의 question_text를 출력
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
