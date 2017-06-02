8ec14db37f8ebebf5a7d28b0bb81afa7ddd3b6f9
Seungha Lee <poopa8788@gmail.com>
2017-06-02 11:21:11 +0900

    modify index view(use template), add template(polls/index.html)

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # latest_question_list라는 키로 위 쿼리셋을 전달
+    # polls/index.html을 이용해 render 결과를 리턴
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # Template Does Not Exist
+    # settings.py에서
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the result of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're votiong on question %s." % question_id)
